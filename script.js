cf2662edc1cfa9a7e22868a1ed75a3fd51028aac
Pijus <pijus.jurciukonis@gmail.com>
2022-10-31 18:41:27 +0200

    Create calculator structure

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Determine which function to use based on given operator
 function operate(operator, a, b) {
     switch (operator) {
         case '+': return add(a, b);
@@ -7,18 +8,22 @@ function operate(operator, a, b) {
     };
 }
 
+// Add numbers
 function add(a, b) {
     return a + b;
 };
 
+// Subtract numbers
 function subtract(a, b) {
     return a - b;
 };
 
+// Multiply numbers
 function multiply(a, b) {
     return a * b;
 };
 
+// Divide numbers
 function divide(a, b) {
     return a / b;
 };
